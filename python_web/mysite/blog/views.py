c52b6ef7026719d2ce886fdee69a6b17593da82c
piaopiao9393 <zhoushengpu@126.com>
2017-03-23 23:40:35 +0800

    等下detail页面，未完成（有bug）

--- python_web/mysite/blog/views.py
+++ python_web/mysite/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from django.shortcuts import render,get_object_or_404
 # Create your views here.
 def post_list(request):
     posts = Article.objects.filter(published_date__isnull=False).order_by('-published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+def post_detail(request,pk):
+    post = get_object_or_404(Article,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
--- python_web/mysite/mysite/urls.py
+++ python_web/mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.post_list)
+    url(r'^$',views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail),
 ]
