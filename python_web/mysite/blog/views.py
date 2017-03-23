d4881d4cf8f6c9d55bb3db12e9bd067ad381fede
piaopiao9393 <zhoushengpu@126.com>
2017-03-23 17:00:54 +0800

    使用bootstrap美化页面

--- python_web/mysite/blog/views.py
+++ python_web/mysite/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+def post_list(request):
+    posts = Article.objects.filter(published_date__isnull=False).order_by('-published_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
--- python_web/mysite/mysite/settings.py
+++ python_web/mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR),'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+STATICFILES_DIRS=(os.path.join(BASE_DIR,"static"),)
 
 
 # Internationalization
